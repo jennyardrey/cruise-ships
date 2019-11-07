25e4ab266c9da95af1dd8ad83e220230dd4758c3
Jenny Ardrey <jenny@ardrey.co.uk>
2019-11-07 14:20:57 +0000

    added abilty to set sail

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,3 +11,21 @@ describe('constructor', () => {
 		expect(newShip.startingPort).toBe('Dover');
 	})
 });
+
+describe('setSail', () => {
+	const newShip = new Ship('Titanic', 'Dover');
+	newShip.setsSail();
+	it('sets sail from port', () => {
+		expect(newShip.startingPort).toBeFalsy();
+	})
+})
+
+/* describe('setSail', () => {
+	const newShip = new Ship('Titanic', 'Dover');
+	it('sets sail from a port', () => {
+		expect(newShip.ports).toBeInstanceOf(Array);
+		newShip.setSail('Portsmouth');
+		expect(newShip.ports).toEqual(['Dover', 'Portsmouth'])
+	})
+	
+}) */
--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ class Ship {
 	constructor(name, startingPort) {
 		this.name = name,
 		this.startingPort = startingPort
+	
 	};
+	setsSail(){
+		this.startingPort = null;
+	}
+
 }
 module.exports = Ship;
+	// this.ports = [startingPort];
+/* 
+	dock(port){
+		this.ports.push(port);
+	} */
